a81f58f9bb2a954168d1465d52295ed1f9b3014f
Philani Manatha <philanimanatha@outlook.com>
2023-07-14 19:25:02 +0000

    4-puts.c1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* prints a message*/
+int main(void)
+{
+	/*print a string*/
+	char str[] = "Programming is like building a multilingual puzzle:\n";
+
+	puts(str);
+	return (0);
+}

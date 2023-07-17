408bc00db7b76f223207131512177ce2be4c3b3a
Philani Manatha <philanimanatha@outlook.com>
2023-07-17 13:43:06 +0000

    2-print_alphabets1

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  *main - Prints alphabets in lowercase, followed by a new line
+  *Description: This program prints alphabets in lower case using putchar
+  *you can only use putchar twice in your code
+  *Return: This function returns 0
+  */
+
+int main(void)
+{
+	/**
+	  * Declaring the character variable that will be used for different
+	  * alphabets
+	  */
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+	return (0);
+}

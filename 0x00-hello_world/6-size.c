7023598579f3899d2d3e64903b08ad66d4bc3f8b
Philani Manatha <philanimanatha@outlook.com>
2023-07-17 21:28:16 +0000

    6-size.c1

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - It prints
+  *Description: uses printf statement to print
+  *Return: returns 0
+  */
+
+int main(void)
+{
+	printf("Size of a char: %lu", sizeof(char));
+	printf("Size of a char: %lu", sizeof(char));
+	printf("Size of a char: %lu", sizeof(char));
+	printf("Size of a char: %lu", sizeof(char));
+	return (0);
+}

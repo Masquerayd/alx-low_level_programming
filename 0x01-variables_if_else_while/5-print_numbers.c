b08739e19046fce9e14eb877147c2f7b2da847a1
Philani Manatha <philanimanatha@outlook.com>
2023-07-17 20:54:10 +0200

    5-print_numbers1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - it ptint numbers 0 to 
+ * Description: uses printf to print numbers
+ * Return: main function returns 
+ */
+int main(void)
+{
+	int num;
+
+	for(num = 0; num <= 9; num++)
+	{
+		printf("%i", num);
+	}
+printf("\n");
+return (0);
+}

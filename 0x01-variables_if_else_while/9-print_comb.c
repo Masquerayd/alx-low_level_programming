808cbdcb366ccb06a6da2373390fe2fa36da3ab6
Philani Manatha <philanimanatha@outlook.com>
2023-07-18 00:40:21 +0000

    9-print_comb,c1

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - it ptint numbers 0 to
+ * Description: uses printf to print numbers
+ * Return: main function returns
+ */
+int main(void)
+{
+	int hex = 0x00;
+
+	for (hex = 0 ; hex <= 9 ; hex++)
+	{
+
+		putchar('0' + hex);
+		if (hex != 9)
+		{
+			putchar(',');
+		}
+	}
+
+putchar('\n');
+return (0);
+}

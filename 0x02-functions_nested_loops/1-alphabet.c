bec5233ec021e740f1c1a9276b7641388da411de
Philani Manatha <philanimanatha@outlook.com>
2023-07-18 23:54:29 +0000

    1-alphabet.c1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "_putchar.c"
+
+
+
+
+void print_alphabet()
+{
+	char A ;
+
+	for ( A = 'a'; A <= 'z'; A++)
+	{
+		_putchar(A);
+
+	}
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *main - test code
+  *Description: Used to test functions defined in main.h
+  *Return: return 0
+  */
+
+int main(void)
+{
+	print_alphabet;
+	return (0);
+}

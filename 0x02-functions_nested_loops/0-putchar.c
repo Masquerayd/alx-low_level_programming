84bab7543e745a72e1815917191713379181be59
Philani Manatha <philanimanatha@outlook.com>
2023-07-18 20:48:22 +0000

    0-putchar.c1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *main - prints
+  *Description: prints _putchar using header main.h
+  *Return: Returns 0
+  */
+
+int main(void)
+{
+	int i = 0;
+	char string[] = "_putchar\n";
+
+	while (i < 9)
+	{
+
+		_putchar(string[i]);
+		i++;
+	}
+
+	return (0);
+
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+#Functions, nested loops
+
+##objectives
+
+In this folder we willl be looking at nested loop , functions different between a declaration and a definition of a function, what is a prototype , the scope of variables and header files
+

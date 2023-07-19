222b706b7a6170db19ce42ff9d87beaa3d259e7d
Philani Manatha <philanimanatha@outlook.com>
2023-07-19 14:04:54 +0000

    5-sign.c4

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,19 +3,19 @@
 /**
   * print_sign - detects sign
   * @n: int variable n passed in function
-  * Description: detects the sign of a variable that is passed into the function
+  * Description: detect the sign of a variable that is passed into the function
   * Return: return 1, 0 , -1
   */
 int print_sign(int n)
 {
 	if (n < 0)
-	{
-		return (-1);
+	{	_putchar('-');
+		return (1);
 	}
 	else if (n == 0)
 	{
 		return (0);
 	}
-
-	return (+1);
+	_putchar('+');
+	return (1);
 }

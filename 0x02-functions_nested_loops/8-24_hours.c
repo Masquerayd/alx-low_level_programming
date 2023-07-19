e0bdd6b26c14f4096534fb6af1bc3ea21362078e
Philani Manatha <philanimanatha@outlook.com>
2023-07-19 22:59:12 +0000

    8-24_hours.c2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,15 +13,16 @@ void jack_bauer(void)
 
 	for (H = 0; H <= 2; H++)
 	{
-		_putchar('0' + H);
 		for (HH = 0; HH <= 9; HH++)
 		{
-			_putchar('0' + HH);
 			for (M = 0; M <= 6; M++)
 			{
-				_putchar('0' + M);
 				for (MM = 0; MM <= 9; MM++)
 				{
+					_putchar('0' + H);
+					_putchar('0' + HH);
+					_putchar(':');
+					_putchar('0' + M);
 					_putchar('0' + MM);
 				}
 			}

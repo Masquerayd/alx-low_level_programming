62fb0aff42a270b8d7b2e945b550f2241e51679f
Philani Manatha <philanimanatha@outlook.com>
2023-07-20 23:55:11 +0000

    0-isupper.c1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *_isupper - checks if a letter is a capital letter
+  *
+  *Decription: returns 1 if captital and returns 0 if small letter
+  * @c: int c
+  *Return: return 0 and 1
+  */
+int _isupper(int c)
+{
+	char ch; /* variable init*/
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		if (ch == c)
+		{
+			return (1); /* returns 1 if alphabet is a capital*/
+		}
+	}
+	return (0); /* returns 0 if aplhabet is not a capital*/
+
+}

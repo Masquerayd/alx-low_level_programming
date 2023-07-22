4155818f0bfd5de998a436496b407514fdc87126
Philani Manatha <philanimanatha@outlook.com>
2023-07-22 00:24:06 +0000

    3-convert_day.c_first_test

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,10 @@
 
 int positive_or_negative(int a);
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+
 
 #endif 
 

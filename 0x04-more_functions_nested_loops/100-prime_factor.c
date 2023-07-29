a8f908e9e0069aa99b5b7d4cb2f947a9f79e5fdf
Philani Manatha <philanimanatha@outlook.com>
2023-07-29 18:49:52 +0000

    print_largest1

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,30 +1,30 @@
-#include <stdio.h> 
+#include <stdio.h>
 
+
+/**
+  *main - print largest prime number
+  *Return: always (0)
+  */
 int main(void)
 {
 	int number = 1231952;
 	int oddn;
 
 
-	while(number%2 == 0)
-	{	
-		printf("2,");
-		number = number/2;
+	while (number % 2 == 0)
+	{
+		number = number / 2;
 	}
 
-	for(oddn = 3; oddn*oddn <=number; oddn += 2)
+	for (oddn = 3; oddn * oddn <= number; oddn += 2)
 	{
-		while (number%oddn == 0)
+		while (number % oddn == 0)
 		{
-			printf("%d,", oddn);
-			number = number/oddn;
+			number = number / oddn;
 		}
 	}
 
-	if (number > 1)
-	{
-		printf("%d", number);
-	}
+	printf("%d", number);
 	printf("\n");
 	return (0);
 

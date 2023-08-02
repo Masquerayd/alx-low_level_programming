faef65e42fd88512e405e1e976c6c0ce3e8385a3
Philani Manatha <philanimanatha@outlook.com>
2023-08-02 23:39:32 +0000

    print_rec_attempt_1

---
+++ 0x08-recursion/1-print_rec_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+  * _print_rev_recursion - prints a string
+  * @s: char pointer s
+  * Return: always null
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(*s);
+	return;
+
+}

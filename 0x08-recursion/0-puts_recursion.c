2dca557eefab6f9f7b63881f0a3da4d7828b86a8
Philani Manatha <philanimanatha@outlook.com>
2023-08-03 00:11:44 +0000

    print_recursion_attempt_4

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,16 +8,17 @@
   */
 void _puts_recursion(char *s)
 {
-	if (*s != 32)
-	{
-		_putchar(*s);
-	}
-	s++;
+	int i = 0;
+
 	if (*s != '\0')
 	{
-		_puts_recursion(s + 1);
+
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+		i = 1;
 	}
-	if (*s == '\0')
+	if (*s == '\0' && i == 0)
 	{
 		_putchar('\n');
 	}

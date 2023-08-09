a4c0223586b450dce37732253a4610086c59f74f
Philani Manatha <philanimanatha@outlook.com>
2023-08-09 21:42:33 +0000

    first attempt for Reverse_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - Reverse the content of an array of integers
+ *
+ * @a: int a is a array of integers
+ * @n: int n is the number of elements of the array
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int initial = 0;
+
+	while (initial < n / 2)
+	{	
+		temp = a[initial];
+
+		a[initial] = a[n - initial - 1];
+		
+		a[n - initial - 1] = temp;
+		initial++;
+	}
+}

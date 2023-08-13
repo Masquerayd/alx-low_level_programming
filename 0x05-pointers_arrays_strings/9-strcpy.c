1736b7f69727250343cbf56d1fe028d4f9d9620a
Philani Manatha <philanimanatha@outlook.com>
2023-08-13 01:32:32 +0000

    solved error of not coping for strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcpy - copies string src to dest
@@ -12,20 +11,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
 	char temp[10000] = {0};
-	int length = 0;
-
-	while (*(dest + length) != '\0')
-	{
-		length++;
-	}
-
-	printf(" length: %d\n", length);
 
 	while (*(src + count) != '\0')
 	{
 		temp[count] = *(src + count);
-		printf("temp: %c\n", temp[count]);
-		*(dest + length -1) = temp[count];
+		*(dest + count) = temp[count];
 		count++;
 	}
 	return (dest);

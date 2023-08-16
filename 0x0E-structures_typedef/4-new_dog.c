769db5bcbbfb57c4709f51ec34a62b32f54105ab
Philani Manatha <philanimanatha@outlook.com>
2023-08-16 01:02:45 +0000

    first attempt for 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - store dog info in a new struct
+ * @name: dog name
+ * @owner: owners name
+ * @age: dogs age
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+
+	return (dog);
+}

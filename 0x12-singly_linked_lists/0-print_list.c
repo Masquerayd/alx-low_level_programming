6ba345710ee193c3734585ddd47adc59b404ac55
Philani Manatha <philanimanatha@outlook.com>
2023-10-24 12:07:08 +0000

    for Betty_fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,27 +2,28 @@
 #include <stdio.h>
 
 /**
- * printstr - prints the string
+ * printstrl - prints the string
  * @str: string to be printed
+ * @len: length of string
  * Return: nothing
  */
 
 void printstrl(const char *str, unsigned int len)
 {
-        if (str == NULL)
-        {
-                printf("[0] (nil)\n");
-        }
-        else
-        {
-                printf("[%d] %s\n", len, str);
-        }
+	if (str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	else
+	{
+		printf("[%d] %s\n", len, str);
+	}
 }
 
 /**
  * print_list - prints all the elemts of a list_t
  * @h: pointer to the list to be printed
- * Returns: the number of nodes
+ * Return: the number of nodes
  */
 size_t print_list(list_t *h)
 {
@@ -30,7 +31,7 @@ size_t print_list(list_t *h)
 
 	while (h != NULL)
 	{
-printstrl(h->str, h->len);
+	printstrl(h->str, h->len);
 		numNodes++;
 
 

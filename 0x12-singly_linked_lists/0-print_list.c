dd8fa03abab982b8bbc7780302730cb412af8752
Philani Manatha <philanimanatha@outlook.com>
2023-10-24 11:57:40 +0000

    for moved_printstr_to_main_function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 
 /**
+ * printstr - prints the string
+ * @str: string to be printed
+ * Return: nothing
+ */
+
+void printstrl(const char *str, unsigned int len)
+{
+        if (str == NULL)
+        {
+                printf("[0] (nil)\n");
+        }
+        else
+        {
+                printf("[%d] %s\n", len, str);
+        }
+}
+
+/**
  * print_list - prints all the elemts of a list_t
  * @h: pointer to the list to be printed
  * Returns: the number of nodes

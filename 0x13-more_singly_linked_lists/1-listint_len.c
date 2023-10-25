710742c6cd1b189f6ce31a0fd934de55c1890be6
Philani Manatha <philanimanatha@outlook.com>
2023-10-25 17:30:23 +0000

    first for task2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - counts the number of elements in a list 
+ * @h: pointer address to list
+ * Return: the number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+

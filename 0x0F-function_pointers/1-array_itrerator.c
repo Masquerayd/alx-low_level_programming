c15309c36a37b976346dc6a080a647a75ffc23da
Philani Manatha <philanimanatha@outlook.com>
2023-10-25 15:07:04 +0000

    first_try for task2

---
+++ 0x0F-function_pointers/1-array_itrerator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes the given function on each of the elements of the array
+ * @array - the Array
+ * @size, the size of the array
+ */
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	size_t count = 0;
+
+	if (action == NULL)
+	{
+		return;
+	}
+
+	for ( count = 0; count < size; count++)
+	{
+		(action)(array[count]);
+	}
+}

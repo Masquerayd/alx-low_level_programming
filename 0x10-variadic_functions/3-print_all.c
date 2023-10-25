92432e88e662032ac3b555815054307f9598cc4a
Philani Manatha <philanimanatha@outlook.com>
2023-10-25 11:55:43 +0000

    first_try for TasK3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,64 @@
+#include "variadic_functions.h"
+/**
+ * printstr - prints string
+ * @str: string to check
+ * Return: nothing
+ */
+void printstr(char *str)
+{
+	if (str == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s", str);
+	}
+}
+
+
+/**
+ * print_all - prints indefinte amount of sting , char or number
+ * @format: indicates if its a string , char or number
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+	int count = 0, flag1 = 0;
+	char *str;
+
+	va_start(arg, format);
+
+	while (*(format + count) != '\0')
+	{
+		flag1 = 0;
+		switch (*(format + count))
+		{
+			case 'i':
+				printf("%d", va_arg(arg, int));
+				flag1 = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(arg, double));
+				flag1 = 1;
+				break;
+			case 'c':
+				printf("%c", va_arg(arg, int));
+				flag1 = 1;
+				break;
+			case 's':
+				str = va_arg(arg, char *);
+				printstr(str);
+				flag1 = 1;
+				break;
+		}
+		if (format[count + 1] != '\0' && flag1 == 1)
+		{
+			printf(", ");
+		}
+		count++;
+	}
+	printf("\n");
+	va_end(arg);
+}

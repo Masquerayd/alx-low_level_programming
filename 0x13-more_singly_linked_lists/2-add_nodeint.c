75a030997476f13b7179a0c15fb76e0b50fa7dd4
Philani Manatha <philanimanatha@outlook.com>
2023-10-25 18:14:25 +0000

    3rd_try for Task2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	nodeptr = malloc(sizeof(listint_t));
 
-	if (head == NULL)
+	if (head == NULL  || nodeptr == NULL)
 	{
 		return (NULL);
 	}

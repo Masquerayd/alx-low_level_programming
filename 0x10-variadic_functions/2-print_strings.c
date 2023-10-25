52f56c1d7af47e4eec01192a10b2a8599e2b0386
Philani Manatha <philanimanatha@outlook.com>
2023-10-25 03:07:41 +0000

    first_try for task2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints an indefinte amoung of stings
+ * @separator: the separator for the string
+ * @n: the number of strings
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i = 0;
+	char *str;
+
+	va_start(strings,n);
+
+	while (i < n)
+	{
+		str = va_arg(strings, char *);
+		if( str == NULL)
+		{
+			printf("(nill)");
+		}
+		else 
+		{
+		printf("%s", str);
+		}
+		if ((i + 1) < n && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(strings);
+}

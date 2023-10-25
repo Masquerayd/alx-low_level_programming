7e9b3c3ff7352c87b6f3de59223e7731f2793dd0
Philani Manatha <philanimanatha@outlook.com>
2023-10-25 14:44:29 +0000

    first_try for Task1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,2 +1,13 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - print the name using two types of functions
+ * @name: name to be printed
+ * @f: pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
+
+

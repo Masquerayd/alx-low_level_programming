6899ca21452a99af4eba68d87342064ac77bb757
Philani Manatha <philanimanatha@outlook.com>
2023-11-07 18:40:57 +0000

    structures for Task3

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - create a new dog data
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: a new dog data
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	new = malloc(sizeof(*new));
+
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+
+	return (new);
+}

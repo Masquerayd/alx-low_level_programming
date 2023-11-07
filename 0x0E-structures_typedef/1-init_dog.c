e254edefcbae39c9f61d55427f6e169297367f09
Philani Manatha <philanimanatha@outlook.com>
2023-11-07 16:40:36 +0000

    init_dog.c for task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes variables of dog
+ * @d: pointer to struct called dog
+ * @name: the nam of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+void  init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name;
+	d->age;
+	d->owner;
+}
+
+
+

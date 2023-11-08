9434b0edf899e243e34469b8830b49f4608b570d
Masquerayd <philanimanatha@outlook.com>
2023-11-08 14:40:45 +0000

    for

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name - print the name using two types of functions
- * @name: name to be printed
- * @f: pointer
+ * print_name - prints the name, using argument function
+ * @name: thr name to be printed
+ * @f: the function that will print the name 
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL && name == NULL)
-	{
-		return;
-	}
-	(*f)(name);
+	f(name):
 }
-
-
+	

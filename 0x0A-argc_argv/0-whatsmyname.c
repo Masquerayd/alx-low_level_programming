d75f6538cb954cbf0caa2bfdb154bf0ebf7c80e3
Philani Manatha <philanimanatha@outlook.com>
2023-11-09 10:26:03 +0000

    task0_try2 for argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,11 @@
  * main - the main function
  * @argc: the number of arg
  * @argv: the array of arguments
+ * Return: alwyas zero
  */
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[argc-1]);
-	
-	return 0;
+	printf("%s\n", argv[argc - 1]);
+
+	return (0);
 }

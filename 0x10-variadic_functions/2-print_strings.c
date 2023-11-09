b6d811829172de8355501b1095a68743e9ea3570
Masquerayd <philanimanatha@outlook.com>
2023-11-09 22:09:17 +0000

    task2_try2 for variadic_functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		temp = va_arg(args, char *);
 		if (temp == NULL)
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 		else
 		{

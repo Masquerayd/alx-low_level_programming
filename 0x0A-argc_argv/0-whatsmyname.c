014af1bec99e687594da539728b3f88491241ee6
Philani Manatha <philanimanatha@outlook.com>
2023-11-09 10:21:23 +0000

    task0_try1 for argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function
+ * @argc: the number of arg
+ * @argv: the array of arguments
+ */
+int main(int argc, char * argv[])
+{
+	printf("%s\n",argv[argc-1]);
+	
+	return 0;
+}

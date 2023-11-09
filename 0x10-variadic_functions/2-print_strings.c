0bb174304d9ad2749ac27cbcf4f18757c7c0a07b
Masquerayd <philanimanatha@outlook.com>
2023-11-09 21:47:17 +0000

    task2_try1 for variadic_functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints string in args
+ * @separator: separator
+ * @n: number of args
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *temp;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		temp = va_arg(args, char *);
+		if (temp == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s", temp);
+		}
+		if (i + 1 < n && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}

087e1268aa8f43a3a0d5987d9478a8eb7ed5df1a
Philani Manatha <philanimanatha@outlook.com>
2023-11-09 02:20:32 +0000

    task2_try for function_pointers.h

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,37 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer in the array
+ * @array: the array of integers
+ * @size: the size of the array
+ * @cmp: the function that will be used to search for the array
+ * Return: -1 if not element or the index number
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int count = 0, value = 0;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (0);
+
+	}
+	else if (size <= 0)
+	{
+		return (-1);
+	}
+
+	while (count != size && value == 0)
+	{
+		value = cmp(array[count]);
+
+		if (value != 0)
+		{
+			return (count);
+		}
+		count++;
+	}
+	return (0);
+}
+
